2dd97b2d87870f7cf2939c841428b0e5d2880eea
QSDC-2025 <cliffandjean67@gmail.com>
2026-05-01 10:27:06 -0400

    Add files via upload

--- getNextDance.js
+++ getNextDance.js
@@ -3,8 +3,8 @@ export function getNextDance(danceSchedule) {
     const now = new Date();
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
+    // Loop through the schedule IN ORDER
     for (const entry of danceSchedule) {
-        // Normalize dance date to midnight
         const d = new Date(entry.date);
         const danceDate = new Date(d.getFullYear(), d.getMonth(), d.getDate());
 
